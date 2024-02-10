a74eadb64939f40fa16a038aa11349663dd09381
Ahmed Yehia <shendyahmed49@gmail.com>
2024-02-11 01:13:56 +0200

    addedStyles

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+let header = document.getElementById('header')
+window.onscroll = function(){
+    if(scrollY > 167){
+        header.style.backgroundColor = 'white';
+    }else{
+        header.style.backgroundColor = 'transparent';
+    }
+}
 document.getElementById('menu-btn').addEventListener('click', function() {
     var menu = document.getElementById('menu');
     if (menu.style.display === 'none' || menu.style.display === '') {
@@ -7,3 +15,45 @@ document.getElementById('menu-btn').addEventListener('click', function() {
         menu.style.display = 'none';
     }
 });
+/////////////////////////////////////////////////////////////////////////////////////
+// const cardData =[
+//     {title: 'Travel Agency'  , description: 'First Html, Css project' , link: 'https://ahmedyehia17.github.io/project1/' , imgLink : '../images/TravelAgency.png'}
+// ]
+
+// function createCard(data){
+//     const card = document.createElement('div')
+//     card.classList.add('card')
+
+//     const img=document.createElement('img')
+//     img.classList.add('cardImage')
+//     img.src = data.imgLink;
+//     img.alt = data.title
+
+//     const cardData = document.createElement('div')
+//     cardData.classList.add('card-data')
+
+//     const title = document.createElement('h2')
+//     title.classList.add('card-title')
+//     title.textContent= data.title;
+
+//     const description = document.createElement('p')
+//     description.classList.add('card-description')
+//     description.textContent = data.description;
+
+//     const projectLink = document.createElement('a')
+//     projectLink.classList.add('project-link')
+//     projectLink.href = data.link;
+
+//     cardData.append(title);
+//     cardData.append(description)
+//     cardData.append(projectLink)
+
+//     return card
+// }
+
+// const cardContainer = document.getElementById('cards-container')
+// cardData.map(data =>{
+//     const card = createCard(data);
+//     cardContainer.appendChild(card);
+// })
+
